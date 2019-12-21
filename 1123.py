04cc30828111b6bdae732dae610f4aa05723ab36
romanvelichkin <romanvelichkin1987@gmail.com>
2019-12-21 13:26:35 +0700

    Solved 1123 problem

--- 1123.py
+++ 1123.py
@@ -1,25 +1,73 @@
 # 1123. Зарплата
+# solved
+
+"""
+If right half (reversed) less than left half, we reverse left half and replace the right one with it
+If right half (reversed) is more than left half, we increase left half by 1 and replace it the right one with it
+
+"""
+
 import math
-#input_salary = input()
 
-#input_salary = '00001x20000'
-input_salary = '123456789' #'123454321'
+input_salary = input()
 input_half_len = math.ceil(len(input_salary)/2)
-
+equal_halves = 0
 center_num = ''
+result = ''
 
-if len(input_salary) % 2 == 0:
-    left_half = input_salary[:input_half_len - 1]
-    left_num_end = input_salary[input_half_len - 1:input_half_len]
-    right_num_end = input_salary[input_half_len:input_half_len + 1]
-    print(left_half + ' ' + left_num_end + right_num_end)
+# for salary between 0 and 99
+if len(input_salary) == 1:
+    result = input_salary
+elif len(input_salary) == 2:
+    if int(input_salary[0]) <  int(input_salary[1]):
+        result = str(int(input_salary[0]) + 1) + str(int(input_salary[0]) + 1)
+    elif int(input_salary[0]) >  int(input_salary[1]):
+        result = input_salary[0] + input_salary[0]
+    else:
+        result = input_salary
+
+# for the rest
 else:
-    left_half = input_salary[:input_half_len - 2]
-    left_num_end = input_salary[input_half_len - 2:input_half_len - 1]
-    center_num = input_salary[input_half_len - 1:input_half_len]
-    right_num_end = input_salary[input_half_len:input_half_len + 1]
-    print(left_half + ' ' + left_num_end + center_num + right_num_end)
+    if len(input_salary) % 2 == 0:
+        even_digits = 1
+        left_half = input_salary[:input_half_len]
+        right_half = input_salary[input_half_len:]
+    else:
+        even_digits = 0
+        left_half = input_salary[:input_half_len - 1]
+        center_num = input_salary[input_half_len - 1:input_half_len]
+        right_half = input_salary[input_half_len:]
 
-# а что если не первое правое число больше, а последующие? короче надо больше вариантов, и подумать - все ли они будут решаться одинаково
+    for i in range(len(left_half)):
+
+        if right_half[i] < left_half[-i - 1]:
+            right_half = left_half[::-1]
+            if even_digits == 1:
+                result = left_half + right_half
+            else:
+                result = left_half + center_num + right_half
+            equal_halves = 0
+            break
+
+        elif right_half[i] > left_half[-i - 1]:
+            if even_digits == 1:
+                left_half = str(int(left_half) + 1)
+                right_half = left_half[::-1]
+                result = left_half + right_half
+            else:
+                left_half = str(int(left_half)*10 + int(center_num) + 1)
+                right_half = (left_half[::-1])[1:]
+                result = left_half + right_half
+            equal_halves = 0
+            break
+
+        else:
+            equal_halves = 1
+
+if equal_halves == 1:
+    result = input_salary
+
+print(result)
+
 
 
