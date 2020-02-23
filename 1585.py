6635102efd50fdec3871faeeb5a7f0ca9e0a07ed
romanvelichkin <romanvelichkin1987@gmail.com>
2020-02-23 23:31:51 +0700

    solved problem 1585

---
+++ 1585.py
@@ -0,0 +1,13 @@
+# 1585. Пингвины
+
+n = int(input())
+ping_num = {}
+
+for i in range(n):
+    ping = input()
+    if ping not in ping_num:
+        ping_num[ping] = 0
+    ping_num[ping] += 1
+
+print(max(ping_num, key=ping_num.get))
+
