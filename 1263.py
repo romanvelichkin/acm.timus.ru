b4aa180b7a28854668b82eef070312976da3aede
romanvelichkin <romanvelichkin1987@gmail.com>
2020-02-27 22:29:14 +0700

    solved problem 1263

---
+++ 1263.py
@@ -0,0 +1,23 @@
+# 1263. Выборы
+# solved
+
+data = input().split(' ')
+n = int(data[0])
+m = int(data[1])
+votes = {}
+
+# fill dict with candidates
+# to show candidates without votes
+for i in range(1, n + 1):
+    votes[i] = 0
+
+# votes
+for i in range(m):
+    vote = int(input())
+    votes[vote] += 1
+
+for vote in votes:
+    result = round(100 * votes[vote] / m, 2)
+    print('%.2f' % result + '%')
+
+
